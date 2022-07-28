6bbe6dc2ff5c0aa1274fffca0ea74a21f701a4b3
parkhyeonun <109446777+parkhyeonun@users.noreply.github.com>
2022-07-28 22:20:36 +0900

    20220728

---
+++ ConsoleApp1/ConsoleApp3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int irow = 5;
+
+            for (int i = 0; i < irow; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    System.Console.Write('*');
+                }
+                System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine();
+
+            for (int i = 0; i < irow; i++)
+            {
+                for (int j = irow - i; j > 0; j--)
+                {
+                    System.Console.Write(' ');
+                }
+
+                for (int j = 0; j <= i; j++)
+                {
+                    System.Console.Write('*');
+                }
+                System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine();
+
+            for (int i = 0; i < irow; i++)
+            {
+                for (int j = irow - i; j > 0; j--)
+                {
+                    System.Console.Write(' ');
+                }
+
+                for (int j = 0; j < 1 + ( 2 * i ) ; j++)
+                {
+                    System.Console.Write('*');
+                }
+                System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine();
+
+            for (int i = 0; i < irow; i++)
+            {
+                for (int j = irow - i; j > 0; j--)
+                {
+                    System.Console.Write(' ');
+                }
+
+                for (int j = 0; j < 1 + (2 * i); j++)
+                {
+                    System.Console.Write('*');
+                }
+                System.Console.WriteLine();
+            }
+
+            for (int i = 0; i < irow - 1; i++)
+            {
+                for (int j = 0; j < i + 2; j++)
+                {
+                    System.Console.Write(' ');
+                }
+
+                for (int j = 0;  j < (irow + 2) - (2 * i); j++)
+                {
+                    System.Console.Write('*');
+                }
+                System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine();
+
+            System.Console.WriteLine();
+
+
+
+        }
+    }
+}
